747dadc0d53234d3413e7ea038214a20fd8bd0e9
reubenlsw <limreuben94@gmail.com>
2016-08-25 11:09:23 +0800

    Initial Commit

---
+++ Final Assesment (Reuben)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final Assesment (Reuben)
+//
+//  Created by Nicole on 25/08/2016.
+//  Copyright © 2016 NextAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
